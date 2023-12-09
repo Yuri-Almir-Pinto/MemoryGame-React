e91269a69b6488f5078260393e4d6da8595b663b
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2023-12-09 20:52:31 -0300

    Concertado importação de CSS com caractére em minúsculo no componente 'GameScreen.jsx'.

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,5 +1,5 @@
 import styles from './GameScreen.module.css';
-import ImageCards from './imageCards';
+import ImageCards from './ImageCards';
 import {useState, useEffect} from 'react';
 
 export default function GameScreen(props) {
